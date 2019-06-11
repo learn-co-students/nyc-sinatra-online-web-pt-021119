0ca3e408a4b762970a99c6c6286093593740f6c7
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-11 18:54:21 -0500

    Done.

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -2,7 +2,14 @@ class FiguresController < ApplicationController
   # add controller methods
 
   get '/figures/new' do
+    @titles = Title.all
+    @landmarks = Landmark.all
     erb :'/figures/new'
+  end
+
+  post '/figures' do
+    @figure = Figure.create(name: params["figure"]["name"])
+    binding.pry
   end
 
 end
