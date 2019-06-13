df2e3956c25d34b04a3d6aff30f62065cdd3282b
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-06-12 20:07:14 -0500

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -25,18 +25,16 @@ class LandmarksController < ApplicationController
   end
 
   post '/landmarks' do
-    @landmark = Landmark.create(params['landmark'])
-
-
-    @landmark.save
-    redirect to "/landmarks/#{@landmark.id}"
+    Landmark.create(name: params['landmark']['name'], year_completed: params['landmark']['year_completed'])
+    redirect '/landmarks'
   end
 
   patch '/landmarks/:id' do
-    @landmark = Landmark.find_by_id(params[:id])
-    @landmark.update(params[:landmark])
-
+    @landmark = Landmark.find(params[:id])
+    @landmark.name = params['landmark']['name']
+    @landmark.year_completed = params['landmark']['year_completed']
     @landmark.save
+    
     redirect to "/landmarks/#{@landmark.id}"
   end
 
